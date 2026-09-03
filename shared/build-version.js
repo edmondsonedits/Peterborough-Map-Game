91a4973231acfd389ca1f56ddd58e722d7c67df1
edmondsonedits <edmondsonedits@gmail.com>
2026-09-03 15:00:39 -0400

    v1.6.1 update production build marker

--- shared/build-version.js
+++ shared/build-version.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '1.6.0';
+  const VERSION = '1.6.1';
   const LABEL = `v${VERSION}`;
   const SCRIPT_URL = document.currentScript?.src || new URL('shared/build-version.js', location.href).href;
 
