78efe79bda09ed9992b6c62888b11c85f1e94327
edmondsonedits <edmondsonedits@gmail.com>
2026-09-03 18:05:21 -0400

    v1.6.7 mark Oshawa dispatch calls unavailable

---
+++ cities/oshawa/dispatch-data.js
@@ -0,0 +1,10 @@
+/* Oshawa empty dispatch-data descriptor for base-training preview. */
+(() => {
+  'use strict';
+  const sourceUrl = new URL(document.currentScript?.src || location.href, location.href);
+  window.PTBO_CITY_DISPATCH_SOURCE = Object.freeze({
+    cityId:'oshawa',
+    version:'preview-1',
+    url:new URL('../empty-dispatch.js?v=1.6.7', sourceUrl).href,
+  });
+})();
