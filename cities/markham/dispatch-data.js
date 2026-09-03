8706406044205c8e610f50bb7fb901a94184a7a7
edmondsonedits <edmondsonedits@gmail.com>
2026-09-03 18:05:45 -0400

    v1.6.7 mark Markham dispatch calls unavailable

---
+++ cities/markham/dispatch-data.js
@@ -0,0 +1,10 @@
+/* Markham empty dispatch-data descriptor for base-training preview. */
+(() => {
+  'use strict';
+  const sourceUrl = new URL(document.currentScript?.src || location.href, location.href);
+  window.PTBO_CITY_DISPATCH_SOURCE = Object.freeze({
+    cityId:'markham',
+    version:'preview-1',
+    url:new URL('../empty-dispatch.js?v=1.6.7', sourceUrl).href,
+  });
+})();
