b28b25f9a9b9801b36c4df789149b6ec8c124bb6
edmondsonedits <edmondsonedits@gmail.com>
2026-09-03 20:12:44 -0400

    v1.6.9 sync Pickering city runtime

--- cities/pickering/package.js
+++ cities/pickering/package.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const VERSION='1.6.8';
+  const VERSION='1.6.9';
   const sourceUrl=new URL(document.currentScript?.src || location.href,location.href);
   const config={
     id:'pickering',name:'Pickering',
